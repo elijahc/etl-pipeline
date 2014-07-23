11e40bf6b64e5a7d980845990166fa3cbe3a6bd8
Elijah Christensen <ejd.christensen@gmail.com>
2014-07-23 12:22:34 -0600

    refactored spec naming

--- spec/pipeline_spec.rb
+++ spec/pipeline_spec.rb
@@ -7,6 +7,11 @@ describe Pipeline do
     expect(Pipeline::VERSION).not_to be_nil
     expect(Pipeline::VERSION).to be_instance_of String
   end
+  describe '#new' do
+    it 'returns a new instance of Pipeline' do
+      expect(TestPipeline.new).to be_instance_of TestPipeline
+    end
+  end
 
 
 end
